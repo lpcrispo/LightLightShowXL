517541214b5be2ef4db7308498bdaed7f5248d2f
Louis-Philippe Crispo <lpcrispo@hotmail.com>
2025-09-07 12:18:14 -0400

    Refactor MainBoard class to enhance color sequence management; update fixture properties and improve color transition logic

--- mainboard/mainboard.py
+++ mainboard/mainboard.py
@@ -30,23 +30,26 @@ class MainBoard:
         for fixture_name, fixture in self.available_fixtures.items(): #parcours du fichier JSON
             self.board.append({
                                 "name": fixture_name,
-                                "red": {"id": self.get_channel(fixture_name, "red"), "value": 255},
-                                "green": {"id": self.get_channel(fixture_name, "green"), "value": 255},
-                                "blue": {"id": self.get_channel(fixture_name, "blue"), "value": 255},
                                 "current_type": "sequence", #type de couleur actuelle (sequence, kick)
-                                "current_sequence_color": "white", #couleur actuelle dans la séquence
-                                "current_color_intensity_prc": 1, #intensité actuelle dans la séquence (0-100%)
-                                "next_sequence_color": "black", #couleur suivante dans la séquence
-                                "current_kick_color": "yellow", #couleur actuelle de kick
-                                "current_priority": 0, #priorité actuelle (0=sequence, 1=kick)
-                                "last_kick_color": "yellow", #dernière couleur de kick
-                                "color_start_time": self.last_update_time, #temps de début de la couleur actuelle
-                                "color_duration": 1000, #durée par défaut en milisecondes
-                                "fade_duration": 500, #durée de fondu par défaut en milisecondes
-                                "last_step_time": self.last_update_time
+                                "sequence_red": {"id": self.get_channel(fixture_name, "red"), "value": 255},
+                                "sequence_green": {"id": self.get_channel(fixture_name, "green"), "value": 255},
+                                "sequence_blue": {"id": self.get_channel(fixture_name, "blue"), "value": 255},
+                                "sequence_current_color": "white", #couleur actuelle dans la séquence
+                                "sequence_intensity": 1, #intensité actuelle dans la séquence (0-100%)
+                                "sequence_color_start_time": self.last_update_time, #temps de début de la couleur actuelle
+                                "sequence_color_duration": 1, #durée par défaut en milisecondes
+                                "sequence_fade_duration": 0.5, #durée de fondu par défaut en milisecondes
+                                "sequence_next_color": "black", #couleur suivante dans la séquence
+                                "kick_respond": fixture["kick_respond"], #indique si le kick est activé pour cette fixture
+                                "kick_current_color": "yellow", #couleur actuelle de kick
+                                "kick_activated": False, #indique si le kick est activé
+                                "kick_duration": 0.2, #durée du kick en milisecondes
+                                "kick_red": {"id": self.get_channel(fixture_name, "red"), "value": 0},
+                                "kick_green": {"id": self.get_channel(fixture_name, "green"), "value": 0},
+                                "kick_blue": {"id": self.get_channel(fixture_name, "blue"), "value": 0},
                               })
         for fixture in self.board:
-            print(f"Loaded fixture: {fixture['name']} at DMX addresses {fixture['red']}, {fixture['green']}, {fixture['blue']}")
+            print(f"Loaded fixture: {fixture['name']} at DMX addresses {fixture['sequence_red']['id']}, {fixture['sequence_green']['id']}, {fixture['sequence_blue']['id']}")
 
     def get_channel(self, p_fixture_name, p_channel_name):
        return self.available_fixtures[p_fixture_name]["channels"][p_channel_name]["id"]+self.available_fixtures[p_fixture_name]["dmx_address"]-1
@@ -65,50 +68,46 @@ class MainBoard:
         return sequence[next_idx]
     
     def update_new_color_sequence(self, p_fixture, p_new_color, p_current_time):
-        p_fixture["current_seq_color"] = p_new_color
-        p_fixture["red"]["value"] = self.get_color_r(p_new_color) 
-        p_fixture["green"]["value"] = self.get_color_g(p_new_color) 
-        p_fixture["blue"]["value"] = self.get_color_b(p_new_color)
-        p_fixture["color_start_time"] = p_current_time
+        p_fixture["sequence_current_color"] = p_new_color
+        p_fixture["sequence_red"]["value"] = self.get_color_r(p_new_color) 
+        p_fixture["sequence_green"]["value"] = self.get_color_g(p_new_color) 
+        p_fixture["sequence_blue"]["value"] = self.get_color_b(p_new_color)
+        p_fixture["sequence_color_start_time"] = p_current_time
 
     def apply_intensity_modulation(self, p_fixture):
-        intensity = p_fixture["current_color_intensity_prc"]
-        p_fixture["red"]["value"] = int(p_fixture["red"]["value"] * intensity)
-        p_fixture["green"]["value"] = int(p_fixture["green"]["value"] * intensity)
-        p_fixture["blue"]["value"] = int(p_fixture["blue"]["value"] * intensity)
-    
+        intensity = p_fixture["sequence_intensity"]
+        p_fixture["sequence_red"]["value"] = int(p_fixture["sequence_red"]["value"] * intensity)
+        p_fixture["sequence_green"]["value"] = int(p_fixture["sequence_green"]["value"] * intensity)
+        p_fixture["sequence_blue"]["value"] = int(p_fixture["sequence_blue"]["value"] * intensity)
+
     def update_color_to_next(self, p_fixture, p_current_color, p_new_color, p_percent):
-        p_fixture["red"]["value"] = int(self.get_color_r(p_current_color) + (self.get_color_r(p_new_color) - self.get_color_r(p_current_color)) * p_percent)
-        p_fixture["green"]["value"] = int(self.get_color_g(p_current_color) + (self.get_color_g(p_new_color) - self.get_color_g(p_current_color)) * p_percent)
-        p_fixture["blue"]["value"] = int(self.get_color_b(p_current_color) + (self.get_color_b(p_new_color) - self.get_color_b(p_current_color)) * p_percent)
+        p_fixture["sequence_red"]["value"] = int(self.get_color_r(p_current_color) + (self.get_color_r(p_new_color) - self.get_color_r(p_current_color)) * p_percent)
+        p_fixture["sequence_green"]["value"] = int(self.get_color_g(p_current_color) + (self.get_color_g(p_new_color) - self.get_color_g(p_current_color)) * p_percent)
+        p_fixture["sequence_blue"]["value"] = int(self.get_color_b(p_current_color) + (self.get_color_b(p_new_color) - self.get_color_b(p_current_color)) * p_percent)
 
     def update_board(self):
         current_time = time()
         # Parcours de chaque fixture pour mettre à jour sa couleur
         for fixture in self.board:
-            elapsed = current_time - fixture["color_start_time"]
-            color_duration_s = fixture["color_duration"] / 1000
-            fade_duration_s = fixture["fade_duration"] / 1000
-            begin_fade_percent = (fixture["color_duration"] - fixture["fade_duration"]) / fixture["color_duration"]
-            fade_start = color_duration_s * begin_fade_percent
-            fade_end = fade_start + fade_duration_s
+            elapsed = current_time - fixture["sequence_color_start_time"]
+            color_duration = fixture["sequence_color_duration"] 
+            fade_duration = fixture["sequence_fade_duration"]
+            begin_fade_percent = (color_duration - fade_duration) / color_duration
+            fade_start = color_duration * begin_fade_percent
+            fade_end = fade_start + fade_duration
             # Met à jour la couleur de chaque fixture en fonction du temps écoulé
             # Vérifie si la durée de la couleur actuelle est écoulée
-            if elapsed >= color_duration_s: 
+            if elapsed >= color_duration: 
                 # Passer à la couleur suivante dans la séquence
-                fixture["current_sequence_color"] = fixture["next_sequence_color"]
-                fixture["next_sequence_color"] = self.get_next_color_in_theme_by_type(self.current_theme, fixture["current_type"], fixture["next_sequence_color"])
-                self.update_new_color_sequence(fixture, fixture["current_sequence_color"], current_time)
-                # Ici, tu peux ajouter le code pour envoyer la nouvelle couleur au matériel DMX
-                print(f"Fixture {fixture['name']} changed to color {fixture['current_sequence_color']}")
+                fixture["sequence_current_color"] = fixture["sequence_next_color"]
+                fixture["sequence_next_color"] = self.get_next_color_in_theme_by_type(self.current_theme, "sequence", fixture["sequence_next_color"])
+                self.update_new_color_sequence(fixture, fixture["sequence_current_color"], current_time)
             # Vérifie si la durée de la couleur actuelle est à moitié écoulée
             elif fade_start <= elapsed < fade_end:
-                percent = min((elapsed - fade_start) / fade_duration_s, 1)
-                self.update_color_to_next(fixture, fixture["current_sequence_color"], fixture["next_sequence_color"], percent)
-                print(f"Fading fixture {fixture['name']} to color {fixture['next_sequence_color']} at {percent*100:.1f}%")
+                percent = min((elapsed - fade_start) / fade_duration, 1)
+                self.update_color_to_next(fixture, fixture["sequence_current_color"], fixture["sequence_next_color"], percent)
             else:
                 pass
             self.apply_intensity_modulation(fixture)
-            fixture["last_step_time"] = current_time
         self.last_update_time = current_time
         
